89ba6373a919cd5f65552034b278e9ad714b2afa
koby5i <bobkoby5i@xdeq.com>
2021-03-27 13:55:22 +0100

    initial commit

---
+++ src/test/java/com/koby5i/shop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koby5i.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
