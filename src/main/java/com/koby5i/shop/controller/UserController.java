cb9e88e151b1a6d5de4badc24e7d6699f0c7de54
koby5i <bobkoby5i@xdeq.com>
2021-03-28 23:35:37 +0200

    31 UserController

---
+++ src/main/java/com/koby5i/shop/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.koby5i.shop.controller;
+
+import com.koby5i.shop.model.Role;
+import com.koby5i.shop.model.Transaction;
+import com.koby5i.shop.model.User;
+import com.koby5i.shop.service.ProductService;
+import com.koby5i.shop.service.TransactionService;
+import com.koby5i.shop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping("/api/user/registration")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        if (userService.findByUsername(user.getUsername()) != null){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        //default role
+        user.setRole(Role.USER);
+        return new ResponseEntity<>(userService.saveUser(user),HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/login")
+    public ResponseEntity<?> getUser(Principal principal) {
+        //principal = httpservletrequest.getUserPrincipal();
+        if (principal == null || principal.getName() == null){
+            //logout will also use here so we should return OK httpstatus
+            return  ResponseEntity.ok(principal);
+        }
+        return new ResponseEntity<>(userService.findByUsername(principal.getName()),HttpStatus.OK);
+    }
+
+    @PostMapping("/api/user/purchase")
+    public ResponseEntity<?> purchasedProduct(@RequestBody Transaction transaction){
+        transaction.setPurchaseDate(LocalDateTime.now());
+        return new ResponseEntity<>(transactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/products")
+    public ResponseEntity<?> getAllProducts(){
+        return new ResponseEntity<>(productService.findAllProducts(),HttpStatus.OK);
+    }
+}
