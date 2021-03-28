f15c417187ff20f8bc5b9f724c0b7584951ba79f
koby5i <bobkoby5i@xdeq.com>
2021-03-28 19:48:55 +0200

    26 User service implementation

---
+++ src/main/java/com/koby5i/shop/service/UserService.java
@@ -0,0 +1,19 @@
+package com.koby5i.shop.service;
+
+import com.koby5i.shop.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+
+    User findByUsername(String username);
+
+    List<User> findAllUsers();
+
+    Long numberOfUsers();
+}
