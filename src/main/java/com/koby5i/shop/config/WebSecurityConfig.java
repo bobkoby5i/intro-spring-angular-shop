60e318cdd7ab97a6d6eefbada8af24eaacff4523
koby5i <bobkoby5i@xdeq.com>
2021-03-29 23:18:04 +0200

    39 Web Security Configuration

---
+++ src/main/java/com/koby5i/shop/config/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.koby5i.shop.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Qualifier("userDetailsServiceImpl")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //cross-origin-resource-sharing: localhost:8080 localhost:4200 (allow for it)
+
+        http.cors().and()
+                .authorizeRequests()
+                //these are public path
+                .antMatchers("/resources/**", "/error", "/api/user/**").permitAll()
+                // only admin can reach these API
+                .antMatchers("/api/admin/**").hasRole("ADMIN")
+                //all remaining parths require authentication
+                .anyRequest().fullyAuthenticated()
+                .and()
+                // logout will log the user out by invalidating session
+                .logout().permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/api/user/logout", "POST"))
+                .and()
+                .formLogin().loginPage("/api/user/login")
+                .and()
+                // enable basic authenrication
+                .httpBasic().and()
+                .csrf().disable() ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    //Cross origin resource sharring
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+}
