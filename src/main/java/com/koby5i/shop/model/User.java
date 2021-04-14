2073a53bd48b0d419b23ddca158640ec7901ae06
koby5i <bobkoby5i@xdeq.com>
2021-04-14 21:50:54 +0200

    fix @Column (name = 'name')

--- src/main/java/com/koby5i/shop/model/User.java
+++ src/main/java/com/koby5i/shop/model/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column (name = "names")
+    @Column (name = "name")
     private String name;
 
     @Column (name = "username")
